f313bfffbf95d0cd34914caaec91f1c40230617d
Supakorn <supakorn0728@gmail.com>
2022-11-22 21:27:08 +0700

    initial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
+import Landing from "../components/Landing";
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +11,9 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+      <main>
+        <Landing />
+      </main>
     </div>
   );
 };

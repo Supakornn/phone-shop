c44714879c5b826b4a6f92d277c98987e2473d1c
Supakorn <supakorn0728@gmail.com>
2022-11-26 22:27:17 +0700

    initial

--- pages/api/getCategories.ts
+++ pages/api/getCategories.ts
@@ -1,9 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { sanityClient } from "../../sanity";
+
+const query = groq`*[_type == "category"] {
+  _id,
+
+}`;
 
 type Data = {
   categories: Category[];
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  // const categories = await sanityClient.fetch(query);
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const categories = await sanityClient.fetch(query);
+  console.log(categories);
+  res.status(200).json({ categories });
 }

0abae6545fce71701720b7283f333a6d75f39773
Supakorn <supakorn0728@gmail.com>
2022-11-30 19:58:36 +0700

    initial

--- pages/api/getProducts.ts
+++ pages/api/getProducts.ts
@@ -8,11 +8,11 @@ _id,
 } | order(_createdAt asc)`;
 
 type Data = {
-  products: Product[];
+   products: Product[];
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const products: Product[] = await sanityClient.fetch(query);
-  console.log(products);
-  res.status(200).json({ products });
+   const products: Product[] = await sanityClient.fetch(query);
+   console.log(products);
+   res.status(200).json({ products });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,74 +8,74 @@ import { fetchProducts } from "../utils/fetchProducts";
 import Product from "../components/Product";
 
 interface Props {
-  categories: Category[];
-  products: Product[];
+    categories: Category[];
+    products: Product[];
 }
 
 const Home = ({ categories, products }: Props) => {
-  const showProducts = (category: number) => {
-    return products
-      .filter((product) => product.category._ref === categories[category]._id)
-      .map((product) => <Product product={product} key={product._id} />);
-  };
+    const showProducts = (category: number) => {
+        return products
+            .filter((product) => product.category._ref === categories[category]._id)
+            .map((product) => <Product product={product} key={product._id} />);
+    };
 
-  return (
-    <div className="">
-      <Head>
-        <title>PhoneShop</title>
-      </Head>
+    return (
+        <div className="">
+            <Head>
+                <title>PhoneShop</title>
+            </Head>
 
-      <Header />
+            <Header />
 
-      <main className="relative h-[200vh] bg-[#e7ecee]">
-        <Landing />
-      </main>
-      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]">
-        <div className="space-y-10 py-16">
-          <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
-            New Promos
-          </h1>
-          <Tab.Group>
-            <Tab.List className="flex justify-center">
-              {categories.map((category) => (
-                <Tab
-                  key={category._id}
-                  id={category._id}
-                  className={({ selected }) =>
-                    `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
-                      selected
-                        ? "borderGradient bg-[#35383C] text-white"
-                        : "border-b-2 border-[#35383C] text-[#747474]"
-                    }`
-                  }
-                >
-                  {category.title}
-                </Tab>
-              ))}
-            </Tab.List>
-            <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
-              <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
-              <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
-              <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
-              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
-            </Tab.Panels>
-          </Tab.Group>
+            <main className="relative h-[200vh] bg-[#e7ecee]">
+                <Landing />
+            </main>
+            <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]">
+                <div className="space-y-10 py-16">
+                    <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
+                        New Promos
+                    </h1>
+                    <Tab.Group>
+                        <Tab.List className="flex justify-center">
+                            {categories.map((category) => (
+                                <Tab
+                                    key={category._id}
+                                    id={category._id}
+                                    className={({ selected }) =>
+                                        `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                                            selected
+                                                ? "borderGradient bg-[#35383C] text-white"
+                                                : "border-b-2 border-[#35383C] text-[#747474]"
+                                        }`
+                                    }
+                                >
+                                    {category.title}
+                                </Tab>
+                            ))}
+                        </Tab.List>
+                        <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+                            <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+                            <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+                            <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+                            <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
+                        </Tab.Panels>
+                    </Tab.Group>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default Home;
 
 // Backend Code
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const categories = await fetchCategories();
-  const products = await fetchProducts();
-  return {
-    props: {
-      categories,
-      products
-    }
-  };
+    const categories = await fetchCategories();
+    const products = await fetchProducts();
+    return {
+        props: {
+            categories,
+            products
+        }
+    };
 };

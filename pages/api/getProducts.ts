0c02d00c32fd68643beac41ed2cf2dcf302140b6
Supakorn <supakorn0728@gmail.com>
2022-11-30 21:40:55 +0700

    initial

--- pages/api/getProducts.ts
+++ pages/api/getProducts.ts
@@ -13,6 +13,5 @@ type Data = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
    const products: Product[] = await sanityClient.fetch(query);
-   console.log(products);
    res.status(200).json({ products });
 }
